4b90e583cd1e1de106ad126b7cf2d88a1952f795
rakshit-goel-cse <55257622+rakshit-goel-cse@users.noreply.github.com>
2020-02-13 13:21:14 +0530

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,36 @@
+import random
+comp=['r','p','s']
+c=0
+u=''
+print("ROCK PAPER SCISSORS")
+def enter():
+     global u,c
+     c=(random.randint(0,20))%3
+     print('enter\n*R for rock\n*P for paper\n*S for scisser')
+     u=input('enter your choise\n').lower()
+     print('computer move *',comp[c])
+#all comparisions from now
+def compare():
+     
+     if(comp[c]==u):
+          print('draw')
+     elif(comp[c]=='r' and u=='p'):
+          print('you won')
+     elif(comp[c]=='p' and u=='s'):
+          print('you won')
+     elif(comp[c]=='s' and u=='r'):
+          print('you won')
+     elif(comp[c]=='r' and u=='s'):
+          print('you loss')
+     elif(comp[c]=='p' and u=='r'):
+          print('you loss')
+     elif(comp[c]=='s' and u=='p'):
+          print('you loss')
+     else:
+          print('wrong entry')
+while(1):
+     enter()
+     compare()
+     w=input('want to play again:-\npress n to exit or any key to continue').lower()
+     if(w=='n'):
+          break
